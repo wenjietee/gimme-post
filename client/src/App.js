9403080b37ff396e42a3925dacb13b1f05290849
Tee Wenjie <wenjietj@gmail.com>
2023-02-27 16:25:57 +0800

    add ui library and layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { useState } from "react";
 import axios from "axios";
+import { Button, TextField, Box, Typography } from "@mui/material";
+import PropagateLoader from "react-spinners/PropagateLoader";
 function App() {
     const [postPrompt, setPostprompt] = useState("");
     const [apiData, setApiData] = useState({});
-
+    const [isGenerated, setIsGenerated] = useState(true);
+    const [isLoaded, setIsLoaded] = useState(false);
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setIsGenerated(false);
         const config = {
             url: "http://127.0.0.1:3000/create",
             body: { prompt: postPrompt },
@@ -21,6 +25,8 @@ function App() {
             });
             if (status === 200) {
                 setApiData(data);
+                setIsLoaded(true);
+                setIsGenerated(true);
             }
         } catch (err) {
             console.log(err);
@@ -33,31 +39,70 @@ function App() {
 
     return (
         <div className="App">
+            <Box
+                display="flex"
+                component="div"
+                justifyContent="center"
+                alignItems="center"
+                minHeight="30vh"
+                sx={{ flexDirection: "column" }}
+            >
+                <Typography variant="h4" gutterBottom>
+                    Gimme-Post! 🤖
+                </Typography>
+                <Typography
+                    variant="body2"
+                    sx={{ fontStyle: "italic" }}
+                    gutterBottom
+                >
+                    All in one post generator (Captions, Image, Hashtags) using
+                    prompts!
+                </Typography>
+                <TextField
+                    id="filled-basic"
+                    label="Enter a Prompt"
+                    variant="filled"
+                    value={postPrompt}
+                    onChange={handleChange}
+                    size="small"
+                    sx={{ m: 2, width: "1000px" }}
+                />
+                <Button
+                    variant="contained"
+                    type="submit"
+                    onClick={handleSubmit}
+                    size="large"
+                    sx={{ m: 2 }}
+                >
+                    Submit
+                </Button>
+            </Box>
+
             <div>
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Prompt:
-                        <input
-                            type="text"
-                            value={postPrompt}
-                            onChange={handleChange}
-                        />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
-            </div>
-            <br />
-            <div>
-                <div>{apiData?.hashtags?.choices[0].text}</div>
-                <div>{apiData?.captions?.choices[0].text}</div>
-                <div>
-                    <img
-                        src={apiData?.image?.data[0].url}
-                        alt="openai"
-                        width="512"
-                        height="512"
-                    />
-                </div>
+                {isGenerated ? (
+                    <>
+                        <div>
+                            {isLoaded ? apiData.hashtags.choices[0].text : null}
+                        </div>
+                        <br />
+                        <div>
+                            {isLoaded ? apiData.captions.choices[0].text : null}
+                        </div>
+                        <br />
+                        <div>
+                            {isLoaded ? (
+                                <img
+                                    src={apiData?.image?.data[0].url}
+                                    alt="openai"
+                                    width="512"
+                                    height="512"
+                                />
+                            ) : null}
+                        </div>
+                    </>
+                ) : (
+                    <PropagateLoader />
+                )}
             </div>
         </div>
     );
